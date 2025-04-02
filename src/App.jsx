ce5983de0735407c98602dbee89d90e51bcf540c
xcyberpunkx0 <xcyberpunkx0@gmail.com>
2025-04-03 01:18:03 +0530

    navbar bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import Navbar from './components/NavBar'
+import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import CompanyLogo from './components/CompanyLogo'
 import PurposeSection from './components/PurposeSection'
